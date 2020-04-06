63982a27ea1ba2c4ba0c875e587319e972b2b9a4
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-04-06 09:58:54 +0100

    add external fetch api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,9 +6,18 @@ export function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
+    const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+
+    const apiKey = "&appid=1c99b7dbe2469eebf9caea877d515b0a";
+
+      const url = baseURL + 90001 + apiKey;
+      fetch(url).then(res => res.json())
+      .then(data => {
+        document.getElementById('results').innerHTML = `The weather condition in ${data.name} is ${data.weather[0].description}`
+      })
 }
